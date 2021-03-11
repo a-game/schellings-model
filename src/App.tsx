722b9e688b6adcb13f760d98c318b8a4972c0792
Ahmad Game <me@ahmadgame.com>
2021-03-11 20:17:10 +0100

    better defaults

--- src/App.tsx
+++ src/App.tsx
@@ -32,17 +32,18 @@ function logCellStatus(arr: Nullable<Cell>[]) {
 }
 
 function App() {
-  const _tickDelay = 500; // ms
+  const _tickDelay = 300; // ms
   const _width = 50;
   const _area = Math.pow(_width, 2);
 
-  const [tolerens, setTolerens] = useState(0.6);
-  const [emptyPercent, setEmptyPercent] = useState(0.2);
+  const [tolerens, setTolerens] = useState(0.7);
+  const [emptyPercent, setEmptyPercent] = useState(0.1);
   const [xPercent, setXPercent] = useState(0.2);
 
   const [running, setRunning] = useState(false);
   const [cells, setCells] = useState(Array<Nullable<Cell>>());
   const [unhappyCount, setUnhappyCount] = useState(0);
+  const [tickCount, setTickCount] = useState(0);
 
   useLayoutEffect(() => reset(), [tolerens, emptyPercent, xPercent]);
 
@@ -59,7 +60,7 @@ function App() {
             .filter((c) => c.kind !== cell.kind);
 
           if (different.length / neighbours.length > tolerens) {
-            unhappy = unhappy + 1;
+            unhappy++;
             const newHome = getRandomEmptyIndex(cells);
             swap(copy, index, newHome);
           }
@@ -71,17 +72,16 @@ function App() {
       };
     }
 
-    if (!running) {
-      return;
-    }
+    if (!running) return;
 
     setTimeout(() => {
       const res = tick();
-      setUnhappyCount(res.unhappyCount);
-      // Nothing has changes and everybody is happy.
+      // Nothing changed so everybody must be happy.
       if (cells.every((value, index) => value === res.newState[index])) {
-        setRunning(false);
-      } else {
+        stop();
+      } else if (running) {
+        setTickCount((v) => v + 1);
+        setUnhappyCount(res.unhappyCount);
         setCells(res.newState);
       }
     }, _tickDelay);
