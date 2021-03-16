07a2ea67ea74b91bd93da204a29fc35436ac8a2f
Ahmad Game <me@ahmadgame.com>
2021-03-16 09:16:02 +0100

    Made it into a react component

--- README.md
+++ README.md
@@ -1,1 +1,23 @@
-# Simulation for Schelling's segregation model
+# schellings-model-sim
+
+A React components that simulates Schelling's Segregation model.
+
+## How to use
+
+Clone this repo to your local computer, then run:
+
+- `npm install && npm run build`
+
+- To make this component available to other projects on your local computer, run `yarn link`.
+- Then go to the project where you want to use this package and run `yarn link "schellings-model-sim"`.
+
+Finally, to fix the multiple copies of React bug that shows up with linked React packages:
+
+- navigate to the root of the `schellings-model-sim` package
+- run `npm link "../path/to/your/project/mode_modules/react"`
+
+You can now import `schellings-model-sim` as a normal package installed from npm like so:
+
+```
+import Model from 'schellings-model-sim'
+```
--- src/index.tsx
+++ src/index.tsx
@@ -1,17 +1,220 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.scss';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import * as React from 'react';
+import './styles.scss';
+import {
+  Nullable,
+  notEmpty,
+  getRandomEmptyIndex,
+  swap,
+  getGridNeighbours,
+} from './util';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+const { useState, useEffect, useRef, useLayoutEffect } = React;
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+enum Kind {
+  X,
+  O,
+}
+
+interface Cell {
+  kind: Kind;
+}
+
+interface TickResult {
+  newState: Nullable<Cell>[];
+  unhappyCount: number;
+}
+
+function logCellStatus(arr: Nullable<Cell>[]) {
+  console.log(`
+    Total:  ${arr.length},
+    X:      ${arr.filter((c) => c?.kind === Kind.X).length},
+    O:      ${arr.filter((c) => c?.kind === Kind.O).length},
+    Empty:  ${arr.filter((c) => c === null).length}
+`);
+}
+
+const Simulation: React.FC = () => {
+  const _tickDelay = 300; // ms
+  const _width = 50;
+  const _area = Math.pow(_width, 2);
+
+  const [tolerens, setTolerens] = useState(0.6);
+  const [emptyPercent, setEmptyPercent] = useState(0.2);
+  const [xPercent, setXPercent] = useState(0.2);
+
+  const [running, setRunning] = useState(false);
+  const [shouldReset, triggerReset] = useState(false);
+  const [cells, setCells] = useState(Array<Nullable<Cell>>());
+  const [unhappyCount, setUnhappyCount] = useState(0);
+  const [tickCount, setTickCount] = useState(0);
+  const timerRef = useRef<number>(0);
+
+  useLayoutEffect(() => {
+    function getInitialSetup() {
+      const initial: Nullable<Cell>[] = [];
+
+      for (let i = 0; i < _area; i++) {
+        const x = Math.random();
+
+        if (x <= emptyPercent) {
+          initial.push(null);
+        } else if (x <= (1 - emptyPercent) * xPercent + emptyPercent) {
+          initial.push({ kind: Kind.X });
+        } else {
+          initial.push({ kind: Kind.O });
+        }
+      }
+      return initial;
+    }
+
+    const initial: Nullable<Cell>[] = getInitialSetup();
+    setCells(initial);
+
+    logCellStatus(initial);
+    setUnhappyCount(0);
+    triggerReset(false);
+  }, [_area, emptyPercent, xPercent, shouldReset]);
+
+  useEffect(() => {
+    function tick(): TickResult {
+      const copy = cells.slice();
+      let unhappy = 0;
+
+      cells.forEach((cell, index) => {
+        if (cell === null) return;
+
+        const neighbours = getGridNeighbours(index, cells, _width);
+        const different = neighbours
+          .filter(notEmpty)
+          .filter((c) => c.kind !== cell.kind);
+
+        if (different.length / neighbours.length > tolerens) {
+          unhappy++;
+          const newHome = getRandomEmptyIndex(cells);
+          swap(copy, index, newHome);
+        }
+      });
+      return {
+        newState: copy,
+        unhappyCount: unhappy,
+      };
+    }
+
+    timerRef.current = window.setTimeout(() => {
+      const res = tick();
+      // Nothing changed so everybody must be happy.
+      if (cells.every((value, index) => value === res.newState[index])) {
+        stop();
+      } else if (running) {
+        setTickCount((v) => v + 1);
+        setUnhappyCount(res.unhappyCount);
+        setCells(res.newState);
+      }
+    }, _tickDelay);
+  }, [running, cells, tolerens]);
+
+  const toggle = () => (running ? stop() : run());
+  const run = () => setRunning(true);
+  const stop = () => {
+    window.clearTimeout(timerRef.current);
+    setRunning(false);
+  };
+
+  const renderCells = () => {
+    return cells.map((cell, i) => {
+      let cls = 'sim-cell ';
+      if (cell?.kind === Kind.X) {
+        cls += 'sim-cell__X';
+      } else if (cell?.kind === Kind.O) {
+        cls += 'sim-cell__O';
+      }
+      return <div key={i} className={cls} />;
+    });
+  };
+
+  const renderControls = () => (
+    <div className="sim-controls">
+      <div className="sim-ctrl-group">
+        <button onClick={toggle}>{running ? 'Stop' : 'Start'}</button>
+        <button
+          onClick={() => {
+            stop();
+            setTickCount(0);
+            triggerReset(true);
+          }}
+        >
+          Reset
+        </button>
+      </div>
+
+      <div className="sim-ctrl-group">
+        <div className="sim-sliders">
+          <label htmlFor="tolerensSlider">Tolerens</label>
+          <input
+            id="tolerensSlider"
+            type="range"
+            min={0}
+            max={1}
+            step={0.1}
+            value={tolerens.toString()}
+            onChange={(e) => setTolerens(parseFloat(e.target.value))}
+          />
+          <label htmlFor="tolerensSlider">{tolerens * 100}%</label>
+
+          <label htmlFor="emptySlider">Empty cells</label>
+          <input
+            id="emptySlider"
+            type="range"
+            min={0}
+            max={1}
+            step={0.1}
+            value={emptyPercent.toString()}
+            onChange={(e) => {
+              stop();
+              setEmptyPercent(parseFloat(e.target.value));
+            }}
+          />
+          <label htmlFor="emptySlider">{emptyPercent * 100}%</label>
+
+          <label htmlFor="xPercentSlider">Red population</label>
+          <input
+            id="xPercentSlider"
+            type="range"
+            min={0}
+            max={1}
+            step={0.1}
+            value={xPercent.toString()}
+            onChange={(e) => {
+              stop();
+              setXPercent(parseFloat(e.target.value));
+            }}
+          />
+          <label htmlFor="xPercentSlider">{xPercent * 100}%</label>
+        </div>
+      </div>
+    </div>
+  );
+
+  return (
+    <div className="sim">
+      {renderControls()}
+      <div
+        id="sim-grid"
+        className="sim-grid"
+        style={{
+          gridTemplateColumns: `repeat(${_width}, 10px)`,
+        }}
+      >
+        {renderCells()}
+      </div>
+      <label htmlFor="sim-grid" className="sim-grid__label">
+        {`Ticks: ${tickCount} — Unhappy cells: ${unhappyCount} (${(
+          (unhappyCount / cells.length) *
+          100
+        ).toFixed(1)}%)`}
+      </label>
+    </div>
+  );
+};
+
+export default Simulation;
